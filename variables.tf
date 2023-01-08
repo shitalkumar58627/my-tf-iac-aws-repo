ce207ec457651b5eb0a6fce4b5f706f0279a8e46
shitalkumar58627 <53770105+shitalkumar58627@users.noreply.github.com>
2023-01-08 13:26:18 +0530

    Update variables.tf
    changed key

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "default-ec2.pem"
+  default     =  "key1"
 }
 
 variable "instance_type" {
